ef580480f8a023ab427b067d62820ee548e2283f
mohandoo <mohandmeghaoui0@gmail.com>
2025-07-14 12:15:29 +0100

    Update

--- src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
+++ src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
@@ -1,15 +1,20 @@
 package com.mohand.SchoolManagmentSystem;
 
 import com.mohand.SchoolManagmentSystem.service.azure.AzureBlobService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagmentSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
+	private static final Logger logger = LoggerFactory.getLogger(SchoolManagmentSystemApplication.class);
 
+	public static void main(String[] args) {
+		logger.info("✅ Logging test!");
+		logger.info("📁 Working directory: {}", System.getProperty("user.dir"));
+		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
 	}
 
 
