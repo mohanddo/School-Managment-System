c60ccac4e2c245eb73aef9957c94bce5ae1cbb1f
mohandoo <mohandmeghaoui0@gmail.com>
2025-07-14 15:40:49 +0100

    Update

--- src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
+++ src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
@@ -8,12 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagmentSystemApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(SchoolManagmentSystemApplication.class);
-
 	public static void main(String[] args) {
-		logger.info("✅ Logging test!");
-		logger.info("📁 Working directory: {}", System.getProperty("user.dir"));
 		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
 	}
 
