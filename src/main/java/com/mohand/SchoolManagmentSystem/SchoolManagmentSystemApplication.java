fd2e6364a4acdf8a455b2a329ac4e43ba00a12e7
mohandoo <mohandmeghaoui0@gmail.com>
2025-07-14 17:31:06 +0100

    Update

--- src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
+++ src/main/java/com/mohand/SchoolManagmentSystem/SchoolManagmentSystemApplication.java
@@ -5,12 +5,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SchoolManagmentSystemApplication {
+public class SchoolManagmentSystemApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SchoolManagmentSystemApplication.class);
+	}
 
 }
